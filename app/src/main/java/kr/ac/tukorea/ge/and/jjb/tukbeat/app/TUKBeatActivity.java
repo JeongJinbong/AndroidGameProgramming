903ec824ba9e2b310a3cdc34ca8ba152f0f642ce
JeongJinbong <jbong0104@naver.com>
2025-05-08 00:53:48 +0900

    json 파일 생성 및 추가

--- app/src/main/java/kr/ac/tukorea/ge/and/jjb/tukbeat/app/TUKBeatActivity.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/jjb/tukbeat/app/TUKBeatActivity.java
@@ -3,9 +3,9 @@ package kr.ac.tukorea.ge.and.jjb.tukbeat.app;
 import android.os.Bundle;
 
 import kr.ac.tukorea.ge.and.jjb.tukbeat.BuildConfig;
+import kr.ac.tukorea.ge.and.jjb.tukbeat.app.scene.MainScene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
-import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class TUKBeatActivity extends GameActivity {
@@ -15,7 +15,7 @@ public class TUKBeatActivity extends GameActivity {
         GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         Metrics.setGameSize(1600,900);
         super.onCreate(savedInstanceState);
-        new Scene().push();
+        new MainScene().push();
     }
 
 
---
+++ app/src/main/java/kr/ac/tukorea/ge/and/jjb/tukbeat/app/scene/MainScene.java
@@ -0,0 +1,7 @@
+package kr.ac.tukorea.ge.and.jjb.tukbeat.app.scene;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+
+public class MainScene extends Scene{
+
+}
