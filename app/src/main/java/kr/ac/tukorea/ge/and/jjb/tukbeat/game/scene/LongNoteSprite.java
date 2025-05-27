0bccd078ef7c5eac577019d19e76ee9e7511cbb4
JeongJinbong <jbong0104@naver.com>
2025-05-28 04:16:13 +0900

    롱노트 클래스 생성

---
+++ app/src/main/java/kr/ac/tukorea/ge/and/jjb/tukbeat/game/scene/LongNoteSprite.java
@@ -0,0 +1,56 @@
+package kr.ac.tukorea.ge.and.jjb.tukbeat.game.scene;
+
+import kr.ac.tukorea.ge.and.jjb.tukbeat.R;
+import kr.ac.tukorea.ge.and.jjb.tukbeat.data.Note;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IRecyclable;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
+
+public class LongNoteSprite extends Sprite implements IRecyclable {
+    private Note note;
+    private static final float WIDTH = 200f;
+    private static final float SPEED = 450.0f;
+    private static final float LINE_Y = Metrics.height - (50f / 2f) - 150f;
+
+    public static LongNoteSprite get(Note note)
+    {
+        LongNoteSprite lns = Scene.top().getRecyclable(LongNoteSprite.class);
+        if(lns ==null)
+        {
+            lns = new LongNoteSprite();
+        }
+        lns.init(note);
+        return lns;
+    }
+
+    public LongNoteSprite(){
+        super(R.mipmap.slide_note);
+    }
+
+    private void init(Note note) {
+        this.note = note;
+        update();
+    }
+
+    @Override
+    public void onRecycle(){
+        note = null;
+    }
+
+    @Override
+    public void update(){
+        float musicTime = MainScene.scene.getMusicTime();
+
+        float startDiff = note.time - musicTime;
+        float endDiff = note.endTime - musicTime;
+
+        float yTop = LINE_Y - startDiff * SPEED;
+        float yBottom = LINE_Y - endDiff * SPEED;
+
+        float x = note.startX;
+        float height = yBottom - yTop;
+        setPosition(x, yTop + height / 2, WIDTH, height);
+
+    }
+}
